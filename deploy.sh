4c3ab13d30b2a7080d90be68448368d0f3375a68
suntong <suntong@dianmi365.com>
2021-01-30 15:51:20 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 hugo -d docs
-cp -rf docs/* ../stong1994.github.io/docs
+rm -rf ../stong1994.github.io/docs
+cp -rf docs ../stong1994.github.io/docs
 cd ../stong1994.github.io/
-git add * && git commit -m "save article" && git push
+git add * && git commit -m "save article" && git push -f 
 cd -
